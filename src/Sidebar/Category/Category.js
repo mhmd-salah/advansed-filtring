925db0a04f1cdf2a154aeb29d7366b2617cb42e8
mhmd <ms.mhmdsalah@gmail.com>
2024-04-18 01:02:43 +0200

    in porgress

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -2,7 +2,29 @@ import "./Category.css"
 function Category() {
   return (
     <div>
-      category
+      <h2 className="sidebar-title">Category</h2>
+      <div className="category">
+        <label className="sidebar-label-container">
+          <input type="radio" name="test"/>
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test"/>
+          <span className="checkmark"></span>Sneakers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test"/>
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test"/>
+          <span className="checkmark"></span>Sanakals
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test"/>
+          <span className="checkmark"></span>Heals
+        </label>
+      </div>
     </div>
   )
 }
